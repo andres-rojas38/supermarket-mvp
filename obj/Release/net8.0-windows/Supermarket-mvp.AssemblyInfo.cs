30966587e13c5a09a8cd31b1c71ac28e009483c8
andres-rojas38 <piperojas388@gmail.com>
2025-04-10 16:01:13 -0500

    Pay Mode Presenter

--- obj/Release/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb7a02afdc12cce4d926b30eec37dcfc1d5567a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47a8ff0847cd270bd7744c8aa40a0db4eefa9e66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
